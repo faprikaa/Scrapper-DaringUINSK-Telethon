9c3df439659039ee6b60a21c8eca02290bbf1868
faprikaa <93901661+faprikaa@users.noreply.github.com>
2023-02-15 22:31:53 +0700

    Update selenium_login_cookies.py
    add : 
    - membaca file config.ini untuk path driver, download, dan json
    - membaca file config.ini untuk tipe OS
    - membuat file bot_handler
    - memindahkan bot command ke file tersebut
    - menambah fungsi force_cek_jenis_all
    - menambah perintah /fcek
    
    fix :
    - fungsi main dan asyncio
    
    improvement : 
    - baris tambahan pada fungsi cek_jenis_all sehingga menulis ke json setelah mengambil data
    - menulis bagian baru baru pada config.ini

--- selenium_login_cookies.py
+++ selenium_login_cookies.py
@@ -1,5 +1,6 @@
 from lib2to3.pgen2 import driver
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
@@ -12,6 +13,8 @@ from telethon import *
 from lxml import html
 from configparser import ConfigParser
 import nest_asyncio
+import traceback
+from bot_handler import *
 
 # Read config.ini
 config = ConfigParser()
@@ -23,7 +26,10 @@ bot_token = config.get('Telegram_bot', 'bot_token')
 chat = config.getint('Telegram_bot', 'chat')
 username = config.get('Login', 'username')
 password = config.get('Login', 'password')
-jsonfile = "data.json"
+driver_path = config.get('Driver', 'driver_path')
+down_path = config.get('Driver', 'download_path')
+jsonfile = config.get('Driver', 'json_filename')
+OS_type = config.get('Driver', 'OS')
 
 bot = TelegramClient('anon', api_id, api_hash).start(bot_token=bot_token)
 
@@ -31,15 +37,18 @@ total_id = []
 all_id = []
 printed_id = []
 
-options = webdriver.ChromeOptions()
-prefs={"download.default_directory":f"{os.getcwd()}\down"};
-options.add_experimental_option("prefs",prefs);
-ser = Service("driver/chromedriver.exe")
-browser = webdriver.Chrome(service=ser ,options=options)
 nest_asyncio.apply()
 
+options = webdriver.ChromeOptions()
+prefs={"download.default_directory":f"{down_path}", "download.directory_upgrade": True};
+options.add_experimental_option("prefs",prefs);
+if OS_type == "Linux":
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+ser = Service(f"{driver_path}")
+browser = webdriver.Chrome(service=ser ,options=options)
+
 async def sel_login():
-    browser.maximize_window()
     browser.implicitly_wait(5)
     browser.get("https://daring.uin-suka.ac.id");
     browser.delete_cookie("PHPSESSID")
@@ -50,7 +59,7 @@ async def sel_login():
     for i in range(len(old_cookies)):
         if old_cookies[i]["name"] == "PHPSESSID":
             cookiez = old_cookies[i]["value"]
-    time.sleep(5)
+    time.sleep(3)
     global ps
     ps = browser.page_source
     try :
@@ -94,9 +103,8 @@ def take_ss(browser, full_id):
     h = size['height']
     width = x + w
     height = y2 + h
-
     im = Image.open(names)
-    im = im.crop((int(x), int(1), int(width), int(h)))
+    im = im.crop((50, int(1), int(579), int(h)))
     im.save(name)
     return(name)
 
@@ -114,8 +122,6 @@ async def cek_jenis_all():
         total_id.append(full_id)
         pickle.dump(total_id, open("all_id.p", "wb"))
         jenis = cek_jenis(full_id)
-        num_id = full_id.replace("dv-progres-sts-","")
-        div = browser.find_element(By.XPATH, f'//*[@id="{full_id}"]')
         try:
             post = data[f"{full_id}"] 
             if post is None:
@@ -154,6 +160,45 @@ async def cek_jenis_all():
     except:
         pass
 
+    #json write
+    with open(jsonfile, "w") as file:
+        json.dump(data_new, file,  indent=4, sort_keys=True)
+
+async def force_cek_jenis_all():
+    print(all_id)
+    for full_id in all_id:
+        jenis = cek_jenis(full_id)
+        num_id = full_id.replace("dv-progres-sts-","")
+        div = browser.find_element(By.XPATH, f'//*[@id="{full_id}"]')
+        jenis = cek_jenis(full_id)
+        try:
+            if jenis=="Tugas":
+                pic_name = take_ss(browser, full_id)
+                loop.run_until_complete(tugasbot(full_id, pic_name))
+            elif jenis=="Diskusi":
+                pic_name = take_ss(browser, full_id)
+                loop.run_until_complete(diskusibot(full_id, pic_name))
+            elif jenis=="Meeting":
+                pic_name = take_ss(browser, full_id)
+                loop.run_until_complete(meetingbot(full_id, pic_name))
+            else:
+                await bot.send_message(chat, f"Jenis postingan baru : {jenis}")
+        except KeyError:
+            if jenis=="Tugas":
+                pic_name = take_ss(browser, full_id)
+                loop.run_until_complete(tugasbot(full_id, pic_name))
+            elif jenis=="Diskusi":
+                pic_name = take_ss(browser, full_id)
+                loop.run_until_complete(diskusibot(full_id, pic_name))
+            elif jenis=="Meeting":
+                pic_name = take_ss(browser, full_id)
+                loop.run_until_complete(meetingbot(full_id, pic_name))
+            else:
+                await bot.send_message(chat, f"Jenis postingan baru : {jenis}")
+        except Exception as e:
+            await bot.send_message(chat,  f"An error occured, {traceback.format_exc()}")
+    pass
+
 def cek_jenis(full_id):
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     WebDriverWait(browser, 10).until(
@@ -167,7 +212,7 @@ def cek_jenis(full_id):
         print("using sleep") 
         browser.get("https://daring.uin-suka.ac.id/dashboard")
         soup2 = BeautifulSoup(browser.page_source, 'html.parser')
-        time.sleep(10)
+        time.sleep(5)
         main2 = soup2.find("div", {"id": str(full_id)})
         text_main = main2.get_text(" | ", strip = True ).split(" | ")
     jenis = text_main[0]
@@ -208,232 +253,26 @@ def scrape(ps):
     global data
     with open(jsonfile, "r") as file:
         data = json.load(file)
+    global data_new
+    data_new = json_returner(data)
 
-async def tugasbot(full_id, pic_name):
-    # pilih id yang mau dipakai
-    main = soup.find("div", {"id": full_id})
-    status = main.attrs["class"][2]
-    text_a = main.get_text(" | ", strip = True ).split(" | ")
-    sub = main.find("div", {"class": "post_content"})
-    text_b= sub.get_text(" | ", strip = True ).split(" | ")
-    total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
-    itext_b = len(text_b)
-
-    if total_file > 0 :
-        total_file2 = total_file * 2 + 2
-        Node = itext_b - 5 - total_file2
-    else:
-        Node = itext_b - 5
-
-    desc =[]
-    for i in text_b[:Node] : 
-        desc.append(i)
-    desc2 = "\n".join(desc)
-    capt = f"""**jenis : **{text_a[0]}
-**Jurusan :**{text_a[2]}
-**Matkul :**{text_a[3]}
-**Dosen :**{text_a[4]}
-**Deskripsi :**{desc2}
-**Waktu mulai**{text_b[(itext_b-4)]}
-**Waktu mulai**{text_b[(itext_b-2)]}
-**Status : **{status}
-"""
-    await bot.send_file(chat, pic_name, caption=capt)
-
-    #json handler
-    data[f"{full_id}"] = {"jenis" : text_a[0], "Jurusan" : text_a[2], "Matkul" : text_a[3], "Dosen" : text_a[4], "Deskripsi" : desc2, "Waktu mulai" : text_b[(itext_b-4)], "Waktu mulai" : text_b[(itext_b-2)], "Status" : status}
-    with open(jsonfile, "a") as file:
-        json.dump(data, file,  indent=4, sort_keys=True)
-
-    # download file yang ada pada post
-    if total_file == 1:
-        try:
-            file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p/span/a')
-            try:
-                file.click()
-                time.sleep(2)
-                file_upload = await bot.upload_file(f'down/{file_name}')
-                await bot.send_file(chat, file_upload)
-            except Exception as e:
-                print(e)
-                pass  
-        except:
-            raise Exception
-    elif total_file == 0 :
-        await bot.send_message(chat, "No File Attached")
-    else :
-        for i in range(1, total_file +1 ):
-            file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p[{i}]/span/a')
-            link = file.get_attribute("href")
-            clickable = browser.find_element(By.XPATH, f"//a[@href='{link}']")
-            file_name = file.text
-            try :
-                clickable.click()
-                time.sleep(2)
-                file_upload = await bot.upload_file(f'down/{file_name}')
-                await bot.send_file(chat, file_upload)
-            except Exception as e:
-                print(e)
-                pass 
-    
-async def diskusibot(full_id, pic_name):
-    # ambil text
-    main = soup.find("div", {"id": full_id})
-    status = main.attrs["class"][2]
-    text_a = main.get_text(" | ", strip = True ).split(" | ")
-    sub = main.find("div", {"class": "post_content"})
-    text_b= sub.get_text(" | ", strip = True ).split(" | ")
-    total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
-    itext_b = len(text_b)
-
-    if total_file > 0 :
-        total_file2 = total_file * 2 + 1
-        Node = itext_b - 11 - total_file2
-    else:
-        Node = itext_b - 11
-    
-    ##untuk mengantisipasi jika ada lebih dari 1 line pada deskripsi
-    desc =[]
-    for i in text_b[6:Node] : 
-        desc.append(i)
-    desc2 = "\n".join(desc)
-    
-    ## bot caption maker
-    capt = f"""**Jenis :** {text_a[1]}
-**Jurusan :** {text_a[3]}
-**Matkul :  **{text_a[4]}
-**Dosen : **{text_a[5]}
-**Indikator Kemampuan : **{text_b[1]}
-**Materi Perkuliahan : **{text_b[3]}
-**Bentuk Pembelajaran : **{text_b[5]}
-**Deskripsi : **{desc2}
-**Waktu mulai  **{text_b[(itext_b-7)]}
-**Waktu selesai  **{text_b[(itext_b-5)]}
-**Status : **{status}
-"""
-    await bot.send_file(chat, pic_name, caption=capt)
-
-    # json handler
-    data[f"{full_id}"] = {"Jenis" : text_a[1], "Jurusan" : text_a[3], "Matkul" : text_a[4], "Dosen" : text_a[5], "Indikator Kemampuan" : text_b[1], "Materi Perkuliahan" : text_b[3], "Bentuk Pembelajaran" : text_b[5], "Deskripsi" : desc2, "Waktu mulai" : text_b[(itext_b-7)], "Waktu selesai" : text_b[(itext_b-5)], "Status" : status}
-    with open(jsonfile, "w") as file:
-        json.dump(data, file,  indent=4, sort_keys=True)
-
-    # download file yang ada pada post
-    if total_file == 1:
-        try:
-            file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p/span/a')
-            file_name = file.text
-            try:
-                file.click()
-                time.sleep(2)
-                file_upload = await bot.upload_file(f'down/{file_name}')
-                await bot.send_file(chat, file_upload)
-            except Exception as e:
-                await bot.send_message(chat, f"Gagal mengirim file karena {e}")
-        except:
-            raise Exception
-    elif total_file == 0 :
-        await bot.send_message(chat, "No File Attached")
-    else :
-        for i in range(1, total_file + 1 ):
-            file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p[{i}]/span/a')
-            link = file.get_attribute("href")
-            clickable = browser.find_element(By.XPATH, f"//a[@href='{link}']")
-            file_name = file.text
-            try:
-                clickable.click()
-                time.sleep(2)
-                file_upload = await bot.upload_file(f'down/{file_name}')
-                await bot.send_file(chat, file_upload)
-            except Exception as e:
-                await bot.send_message(chat, f"Gagal mengirim file karena {e}")
-
-async def meetingbot(full_id, pic_name):
-    # ambil text
-    main = soup.find("div", {"id": full_id})
-    status = main.attrs["class"][2]
-    text_a = main.get_text(" | ", strip = True ).split(" | ")
-    sub = main.find("div", {"class": "post_content"})
-    text_b= sub.get_text(" | ", strip = True ).split(" | ")
-    total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
-    itext_b = len(text_b)
-
-    if total_file > 0 :
-        total_file2 = total_file * 2 + 2
-        Node = itext_b - 13 - total_file2
-    else:
-        Node = itext_b - 13
-    
-    ##untuk mengantisipasi jika ada lebih dari 1 line pada deskripsi
-    desc =[]
-    for i in text_b[7:Node] : 
-        desc.append(i)
-    desc2 = "\n".join(desc)
-    
-    # print text
-    capt =f"""**Jenis :** {text_a[1]}
-**Jurusan :** {text_a[3]}
-**Matkul :  **{text_a[4]}
-**Dosen : **{text_a[5]}
-**Indikator Kemampuan : **{text_b[1]}
-**Materi Perkuliahan : **{text_b[3]}
-**Bentuk Pembelajaran : **{text_b[5]}
-**Link : **{text_b[6]}
-**Deskripsi : **{desc2}
-**Waktu mulai  **{text_b[(itext_b-11)]}
-**Waktu selesai  **{text_b[(itext_b-13)]}
-**Status : **{status}
-"""
-
-    #send ke telegram via bot
-    await bot.send_file(chat, pic_name, caption=capt)
-
-    # json handler
-    data[f"{full_id}"] = {"Jenis" : text_a[1], "Jurusan" : text_a[3], "Matkul" : text_a[4], "Dosen" : text_a[5], "Indikator Kemampuan" : text_b[1], "Materi Perkuliahan" : text_b[3], "Bentuk Pembelajaran" : text_b[5], "Link" : text_b[6], "Deskripsi" : desc2, "Waktu mulai" : text_b[(itext_b-11)], "Waktu selesai" : text_b[(itext_b-13)], "Status" : status}
-    with open(jsonfile, "w") as file:
-        json.dump(data, file, indent=4, sort_keys=True)
-
-    # download file yang ada pada post
-    if total_file == 1:
-        try:
-            file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p/span/a')
-            file_name = file.text
-            try:
-                file.click()
-                time.sleep(2)
-                file_upload = await bot.upload_file(f'down/{file_name}')
-                await bot.send_file(chat, file_upload)
-            except Exception as e:
-                await bot.send_message(chat, f"Gagal mengirim file karena {e}")
-        except:
-            raise Exception
-    elif total_file == 0 :
-        await bot.send_message(chat, "No File Attached")
-    else :
-        for i in range(1, total_file + 1 ):
-            file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p[{i}]/span/a')
-            link = file.get_attribute("href")
-            clickable = browser.find_element(By.XPATH, f"//a[@href='{link}']")
-            file_name = file.text
-            try:
-                clickable.click()
-                time.sleep(2)
-                file_upload = await bot.upload_file(f'down/{file_name}')
-                await bot.send_file(chat, file_upload)
-            except Exception as e:
-                await bot.send_message(chat, f"Gagal mengirim file karena {e}")
+    send_var(browser, soup, bot, chat)
 
 @bot.on(events.NewMessage(pattern='(?i)/*cek'))
 async def handler(event):
-    loop.run_until_complete(cek_jenis_all())
+    await cek_jenis_all()
 
-def main():
-    global loop
-    loop = asyncio.get_event_loop() # untuk menjalankan async
-    loop.run_until_complete(sel_login()) #login
+@bot.on(events.NewMessage(pattern='(?i)/*fcek'))
+async def handler(event):
+    await force_cek_jenis_all()
+
+async def main():
+    await sel_login() #login
     scrape(browser.page_source) #persiapan beautiful soup
     cek_id() #mengambil data yang ada pada page
-    loop.run_until_complete(cek_jenis_all()) #menganalisis tiap data
-    bot.run_until_disconnected()
+    await cek_jenis_all() #menganalisis tiap data
+    await bot.run_until_disconnected()
 
-main()
+global loop
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
