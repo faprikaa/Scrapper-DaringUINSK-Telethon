3ed675082d43dc20771525a44134da1f77ff09f2
faprikaa <93901661+faprikaa@users.noreply.github.com>
2023-03-02 14:19:17 +0700

    Update main.py
    add:
    - move all bot command to new file
    
    remove:
    - unnercesarry variable and command

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-import asyncio, time
+import asyncio, selenium, time
 from data_handler import *
 from bot_handler import *
 from broweb_handler import *
+from command_handler import *
 from scheduler import *
+from selenium.common.exceptions import *
 
 async def main():
-    global brw, browser, bot, data, arr_id
-    brw = init_browser()
-    browser = await login(brw)
+    global browser, data, arr_id
+    init_browser()
+    browser = await login()
+    init_bot_cmd(browser)
     arr_id = cek_id(browser)
     data = jsonf_read()
-    bot = bot_prepare()
-    msg = await cek_jenis_all(browser, arr_id, data)
-    await send_msg(msg)
-    await bot.run_until_disconnected()
+    # msg = await cek_jenis_all(browser, arr_id, data)
+    # await send_msg(msg)
+    init_schedul(browser, data)
     await looping()
+    await bot.run_until_disconnected()
 
 async def looping():
     while True:
-        schedul(brw, browser, data)
-        asyncio.sleep(600)
-
-@bot.on(events.NewMessage(pattern='(?i)/*cek'))
-async def handler(event):
-    await send_msg("Melakukan Cek")
-    msg = await cek_jenis_all(browser, arr_id, data)
-    await send_msg(msg)
-
-@bot.on(events.NewMessage(pattern='(?i)/*fcek'))
-async def handler(event):
-    await send_msg("Melakukan Force Cek")
-    await force_cek_jenis_all(browser, arr_id)
+        try:
+            await schedul()
+            await asyncio.sleep(600)
+        except :
+            await send_msg(f"An error occured at looping, {traceback.format_exc()}")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
