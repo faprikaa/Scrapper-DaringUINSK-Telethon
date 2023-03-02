388c49dd58f7dff0a29b5cff7123f67f82bff94a
Thoriq Firdaus Arifin <88525167+Rikunz@users.noreply.github.com>
2023-03-02 21:26:25 +0700

    add forum parser

--- data_parser.py
+++ data_parser.py
@@ -9,11 +9,33 @@ def diskusiparser(arr):
         'Indikator Kemampuan :') + 1: arr.index('Materi Perkuliahan :')]
 
     hasil.append(" | ".join(indikatorkemampuan))
-    materiperkuliahan = arr[arr.index('Materi Perkuliahan :')+1:arr.index('Bentuk Pembelajaran :')]
+    materiperkuliahan = arr[arr.index(
+        'Materi Perkuliahan :')+1:arr.index('Bentuk Pembelajaran :')]
     hasil.append(" | ".join(materiperkuliahan))
-    bentuk_pembelajaran = arr[arr.index('Bentuk Pembelajaran :') + 1:arr.index('Bentuk Pembelajaran :')+2]
+    bentuk_pembelajaran = arr[arr.index(
+        'Bentuk Pembelajaran :') + 1:arr.index('Bentuk Pembelajaran :')+2]
     hasil.append(" | ".join(bentuk_pembelajaran))
-    desc = arr[arr.index('Bentuk Pembelajaran :') +2:arr.index('Waktu Mulai')]
+    desc = arr[arr.index('Bentuk Pembelajaran :') + 2:arr.index('Waktu Mulai')]
+    matches = []
+
+    for match in desc:
+        if ".com" in match:
+            matches.append(match)
+
+    for popup in matches:
+        desc.pop(desc.index(popup))
+    hasil.append(" | ".join(desc))
+    return hasil
+
+
+def forumparser(arr):
+    hasil = []
+    jenis = hasil.append(arr[0])
+    jurusan = hasil.append(arr[2])
+    matkul = hasil.append(arr[3])
+    pengirim = hasil.append(arr[1])
+
+    desc = arr[4:arr.index('setuju')-3]
     matches = []
 
     for match in desc:
