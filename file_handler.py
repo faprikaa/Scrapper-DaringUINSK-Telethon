a4b08339d9a6debc998c85478b58b42e739747fe
faprikaa <93901661+faprikaa@users.noreply.github.com>
2023-03-03 09:27:03 +0700

    Update file_handler.py
    add:
    - new method to get file name
    - auto detect download folder
    
    fix:
    - cant download file in Forum post type

--- file_handler.py
+++ file_handler.py
@@ -1,13 +1,13 @@
 import time, os, traceback
 from selenium.webdriver.common.by import By
-import bot_handler
+from  bot_handler import *
 import traceback
 from data_handler import *
 from bot_handler import *
 from broweb_handler import *
 
-send_msg = bot_handler.send_msg
-send_file = bot_handler.send_file
+thisfolder = os.getcwd()
+dl_path = thisfolder + r"//down"
 
 async def files_download(browser, full_id, total_file):
     try:
@@ -15,19 +15,19 @@ async def files_download(browser, full_id, total_file):
             file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p[{i}]/span/a')
             link = file.get_attribute("href")
             clickable = browser.find_element(By.XPATH, f"//a[@href='{link}']")
-            file_name = file.text
-            path =  os.path.isfile("down/" + file_name)
+            filename = get_file_name(file.text)
+            path =  os.path.isfile("down/" + filename)
             if path == False:
                 try :
                     clickable.click()
                     time.sleep(5)
-                    await send_file(f"{file_name}")
+                    await send_file(f"{filename}")
                 except:
                     await send_msg(f"An error occured, {traceback.format_exc()}")
                     pass 
             elif path == True:
                 try:
-                    await send_file(f"{file_name}")
+                    await send_file(f"{filename}")
                 except Exception as e:
                     await send_msg(f"An error occured, {traceback.format_exc()}")
                     pass
@@ -40,22 +40,34 @@ async def file_download(browser, full_id):
         file = browser.find_element(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p/span/a')
         link = file.get_attribute("href")
         clickable = browser.find_element(By.XPATH, f"//a[@href='{link}']")
-        file_name = file.text
-        path =  os.path.isfile("down/" + file_name)
+        filename = get_file_name(file.text)
+        path =  os.path.isfile("down/" + filename)
         if path == False:
             try :
                 clickable.click()
                 time.sleep(5)
-                await send_file(file_name)
+                await send_file(filename)
             except:
                 await send_msg(f"An error occured, {traceback.format_exc()}")
                 pass 
         elif path == True:
             try:
-                await send_file(file_name)
+                await send_file(filename)
             except Exception as e:
                 await send_msg(f"An error occured, {traceback.format_exc()}")
                 pass
         return True
     except :
         return  traceback.format_exc()  
+
+def get_file_name(filename):
+    cut_filename = filename[0:10]
+    try:
+        folder = os.listdir(dl_path)
+        for file in folder:
+            if file.startswith(cut_filename):
+                final_file_name = file
+        return final_file_name
+    except Exception as e:
+        return e
+    
