63377f653847fafe9d2c04bd77ba31c70ce81d6d
faprikaa <93901661+faprikaa@users.noreply.github.com>
2023-03-03 09:33:00 +0700

    scheduler now can work (i hope)
    fix:
    - scheduler function 
    - and many more

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 import datetime, time, calendar, sys
+import pytz
 from data_handler import *
 
 config = ConfigParser()
@@ -11,11 +12,14 @@ Rabu = config.get('jadwal', 'Rabu')
 Kamis = config.get('jadwal', 'Kamis')
 Jumat = config.get('jadwal', 'Jumat')
 
-skrg = datetime.datetime.now()
+time_jakarta =pytz.timezone('Asia/Jakarta')
+skrg = datetime.datetime.now(time_jakarta)
 hari_ini = skrg.strftime("%A" )
 jam_ini = skrg.strftime("%H:%M")
 
-i = 0
+sudah_cek = 0
+waktu_jadwal = False
+send_notif = False
 
 jam_belajar = []
 
@@ -23,51 +27,57 @@ def hari_parser(hari):
     try:
         hari_arr = hari.split(", ")
         return hari_arr
-    except AttributeError: 
-        return hari
     except :
         print(Exception)
 
-def menit_checker(t1, t2, browser, data):
+async def menit_checker(t1, t2):
+    global waktu_jadwal, sudah_cek
     if t1 <= jam_ini <= t2:
         ids = cek_id(browser)
-        cek_jenis_all(browser, ids, data)
-        i = 0
+        cek = await cek_jenis_all(browser, ids)
+
+        if cek == "tidak ada postingan baru":
+            sudah_cek += 1
+        else:
+            await send_msg(f"Sudah melakukan {sudah_cek} cek dan menemukan postingan baru")
+            await send_msg(cek)
+
+        waktu_jadwal = True
+        await time_checker()
+    elif jam_ini == t2 :
+        await send_ms(f"Sudah melakukan {sudah_cek} cek dan saat ini tidak dalam waktu scheduler")
+        time.sleep(60)
+        waktu_jadwal = False
     else :
-        i + 1
+        waktu_jadwal = False
         pass
 
-def checker(hari):
-    if hari != "":
-        arr_jam = hari_parser(hari)
-        if type(arr_jam) == str:
-            cookies_checker(brw, browser)
-            menit_checker(arr_jam[0], arr_jam[1])
-        elif type(arr_jam) == list: 
-            for jam in arr_jam:
-                jams = jam.split(" - ")
-                cookies_checker(brw, browser)
-                menit_checker(jams[0], jams[1])
+async def checker(arr_jam):
+    for jam in arr_jam:
+        jams = jam.split(" - ")
+        # await cookies_checker()
+        await menit_checker(jams[0], jams[1])
 
-def schedul(brws, browsers, datas):
-    global brw, browser, data
-    browser = browsers
-    data = datas
-    brw = brws
-
+async def schedul():
     if hari_ini == "Monday":
-        checker(Senin)
+        hari = hari_parser(Senin)
+        await checker(hari)
     elif hari_ini == "Tuesday":
-        checker(Selasa)
+        hari = hari_parser(Selasa)
+        await checker(hari)
     elif hari_ini == "Wednesday":
-        checker(Rabu)
+        hari = hari_parser(Rabu)
+        await checker(hari)
     elif hari_ini == "Thursday":
-        checker(Kamis)
+        hari = hari_parser(Kamis)
+        await checker(hari)
     elif hari_ini == "Friday":
-        checker(Jumat)
+        hari = hari_parser(Jumat)
+        await checker(hari)
 
-def cookies_checker(brw, browser):
+async def cookies_checker():
     cooki = browser.get_cookies
+
     try:
         for i in range(len(cooki)):
             if cooki[i]["name"] == "PHPSESSID":
@@ -78,6 +88,22 @@ def cookies_checker(brw, browser):
     timestamp_now = calendar.timegm(gmt)
     try :
         if timestamp_now == expiry_daring:
-            browser = login(brw)
+            await send_msg("Cookies habis ketika akan melakukan scrape, mengambil cookies baru")
+            login()
     except:
         pass
+
+async def time_checker():
+    global send_notif
+    if waktu_jadwal == True and send_notif == False:
+        await send_msg("Memasuki waktu jadwal dan auto scrape mode")
+        send_notif = True
+    elif waktu_jadwal == False:
+        send_notif = False
+        pass
+
+async def init_schedul(browsers, datas):
+    global data, browser
+    data = datas
+    browser = browsers
+    await send_msg("Scheduler Berjalan !!")
