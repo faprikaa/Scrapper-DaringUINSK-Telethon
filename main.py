c2b051aa844f5cb19c0375770e97a9baa485c05c
faprikaa <muammarm28@gmail.com>
2023-03-10 20:55:40 +0700

    simplify code

--- main.py
+++ main.py
@@ -7,28 +7,11 @@ from scheduler import *
 from selenium.common.exceptions import *
 
 async def main():
-    global browser, data, arr_id
-    init_browser()
-    browser = await login()
-    init_bot_cmd(browser)
-    arr_id = cek_id(browser)
+    await login()
     data = jsonf_read()
-    # msg = await cek_jenis_all(browser, arr_id, data)
-    # await send_msg(msg)
-    await init_schedul(browser, data)
+    await init_schedul(data)
     await looping()
     await bot.run_until_disconnected()
-
-async def looping():
-    total_cek = 1   
-    while True:
-        try:
-            await schedul()
-            await asyncio.sleep(600)
-            total_cek += 1
-            send_total(total_cek)
-        except :
-            await send_msg(f"An error occured at looping, {traceback.format_exc()}")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
