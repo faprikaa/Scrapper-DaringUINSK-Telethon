eb8d22a5620e6dc0114893327dea12de2898ee8a
faprikaa <muammarm28@gmail.com>
2023-03-10 20:19:15 +0700

    delete unused parameter changelog : - delete refresh page first before call function

--- data_handler.py
+++ data_handler.py
@@ -15,12 +15,14 @@ config.read('config.ini')
 
 jsonfile = config.get('Driver', 'json_filename')
 
+browser = broweb_handler.browser
+
 def jsonf_read():
     with open(jsonfile, "r") as file:
         data = json.load(file)
     return data
 
-def cek_jenis(browser, full_id):
+def cek_jenis(full_id):
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "dv-post-box"))
     )
@@ -46,14 +48,16 @@ def cek_jenis(browser, full_id):
         return jenis
     elif jenis == "Tugas":
         return jenis
+    elif jenis == "Materi":
+        return jenis
     else:
         return (text_main[1])
 
-async def cek_jenis_all(browser, all_id, data=0):
-    if data==0:
+async def cek_jenis_all(all_id=False, data=False):
+    if not data:
         data = jsonf_read()
-    
-    browser.get("https://daring.uin-suka.ac.id/dashboard")
+    if not all_id:
+        all_id = cek_id()  
 
     try:
         browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div/div/nav/ol/li[1]/div/center/h2/b")
@@ -63,43 +67,52 @@ async def cek_jenis_all(browser, all_id, data=0):
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "dv-post-box"))
     )
-
     total_tugas_baru = 0 
     for full_id in all_id:
-        jenis = cek_jenis(browser, full_id)
+        jenis = cek_jenis(full_id)
         try:
             if full_id not in data:
                 if jenis=="Tugas":
-                    data = await tugasbot(browser, full_id, data)
+                    data = await tugasbot(full_id, data)
                     total_tugas_baru = total_tugas_baru + 1
                 elif jenis=="Diskusi":
-                    data = await diskusibot(browser, full_id, data)
+                    data = await diskusibot(full_id, data)
                     total_tugas_baru = total_tugas_baru + 1
                 elif jenis=="Meeting":
-                    data = await meetingbot(browser, full_id, data)
+                    data = await meetingbot(full_id, data)
                     total_tugas_baru = total_tugas_baru + 1
                 elif jenis=="Forum":
-                    data = await forumbot(browser, full_id, data)
+                    data = await forumbot(full_id, data)
+                    total_tugas_baru = total_tugas_baru + 1
+                elif jenis=="Materi":
+                    data = await materibot(full_id, data)
+                    total_tugas_baru = total_tugas_baru + 1
+                elif jenis=="Video":
+                    data = await videobot(full_id, data)
                     total_tugas_baru = total_tugas_baru + 1
                 else:   
                     await send_msg(f"Unknown post type, {jenis}")
             elif full_id in data:
-                status = status_checker(browser, full_id)
+                status = status_checker(full_id)
                 if data[full_id]["Status"] != status:
                     if jenis=="Tugas":
-                        data = await tugasbot(browser, full_id, data)
+                        data = await tugasbot(full_id, data)
                         total_tugas_baru = total_tugas_baru + 1
                     elif jenis=="Diskusi":
-                        data = await diskusibot(browser, full_id, data)
+                        data = await diskusibot(full_id, data)
                         total_tugas_baru = total_tugas_baru + 1
                     elif jenis=="Meeting":
-                        data = await meetingbot(browser, full_id, data)
+                        data = await meetingbot(full_id, data)
                         total_tugas_baru = total_tugas_baru + 1
                     elif jenis=="Forum":
-                        data = await forumbot(browser, full_id, data)
+                        data = await forumbot(full_id, data)
                         total_tugas_baru = total_tugas_baru + 1
-                    else:
-                        await send_msg(f"Unknown post type, {jenis}")
+                    elif jenis=="Materi":
+                        data = await materibot(full_id, data)
+                        total_tugas_baru = total_tugas_baru + 1
+                    elif jenis=="Video":
+                        data = await videobot(full_id, data)
+                        total_tugas_baru = total_tugas_baru + 1
         except Exception as e:
            await send_msg(f"An error occured, {traceback.format_exc()}")
 
@@ -109,35 +122,39 @@ async def cek_jenis_all(browser, all_id, data=0):
 
     # returner
     if total_tugas_baru > 0:
-        hasil = f"ada {total_tugas_baru} tugas baru"
+        hasil = f"ada {total_tugas_baru} postingan baru"
         return hasil
     elif total_tugas_baru == 0:
         hasil = "tidak ada postingan baru"
         return hasil
 
-async def force_cek_jenis_all(browser, all_id):
+async def force_cek_jenis_all(all_id):
     try:
         browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div/div/nav/ol/li[1]/div/center/h2/b")
     except:
-        await login()
+        await send_msg("Cookies habis silahkan login ulang")
 
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "dv-post-box"))
     )
     for full_id in all_id:
-        jenis = cek_jenis(browser, full_id)
-        data= {}
+        jenis = cek_jenis(full_id)
         try:
             if jenis=="Tugas":
-                data = await tugasbot(browser, full_id, data)
+                data = await tugasbot(full_id)
             elif jenis=="Diskusi":
-                data = await diskusibot(browser, full_id, data)
+                data = await diskusibot(full_id)
             elif jenis=="Meeting":
-                data = await meetingbot(browser, full_id, data)
+                data = await meetingbot(full_id)
             elif jenis=="Forum":
-                data = await forumbot(browser, full_id, data)
+                data = await forumbot(full_id)
+            elif jenis=="Materi":
+                data = await materibot(full_id)
+            elif jenis=="Video":
+                data = await videobot(full_id)
             else:
                 await send_msg(f"Unknown post type, {jenis}")
         except Exception as e:
             await send_msg(f"An error occured, {traceback.format_exc()}")
     pass
+
