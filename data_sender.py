ae0500d60f43c164118f083b4f98c83748b8c205
faprikaa <muammarm28@gmail.com>
2023-03-10 20:52:24 +0700

    add new post type

--- data_sender.py
+++ data_sender.py
@@ -5,206 +5,132 @@ from data_handler import *
 from bot_handler import *
 from file_handler import *
 from data_parser import *
+import broweb_handler
 
-async def tugasbot(browser, full_id, data):
+browser = broweb_handler.browser
+
+async def tugasbot(full_id, data=False):
     # pilih id yang mau dipakai
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     main = soup.find("div", {"id": full_id})
     status = main.attrs["class"][2]
     text_a = main.get_text(" | ", strip = True ).split(" | ")
-    sub = main.find("div", {"class": "post_content"})
-    text_b= sub.get_text(" | ", strip = True ).split(" | ")
+    parsered = tugas_parser(text_a)
+    print(text_a)
     total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
-    itext_b = len(text_b)
 
-    if total_file > 0 :
-        total_file2 = total_file * 2 + 2
-        Node = itext_b - 7 - total_file2
-    else:
-        Node = itext_b - 7
-
-    desc =[]
-    for i in text_b[:Node] : 
-        desc.append(i)
-    desc2 = "\n".join(desc)
-
-    if text_b[(itext_b-3)] == "Anda telah mengumpulkan tugas :":
-        wkt_mulai = text_b[(itext_b-6)]
-        wkt_selesai = text_b[(itext_b-4)]
-    else:
-        wkt_mulai = text_b[(itext_b-4)]
-        wkt_selesai = text_b[(itext_b-2)]
-
-    capt = f"""**jenis : **{text_a[0]}
-**Jurusan : **{text_a[2]}
-**Matkul : **{text_a[3]}
-**Dosen : **{text_a[4]}
-**Deskripsi : **{desc2}
+    capt = f"""**HTML-ID : ** `{full_id}`
+**jenis : **{parsered[0]}
+**Jurusan : **{parsered[1]}
+**Matkul : **{parsered[2]}
+**Dosen : **{parsered[3]}
+**Deskripsi : **{parsered[4]}
 **Total File : **{total_file}
-**Waktu mulai**{wkt_mulai}
-**Waktu mulai**{wkt_selesai}
+**Waktu mulai**{parsered[5]}
+**Waktu mulai**{parsered[6]}
 **Status : **{status}
 """
     # take ss of element
-    img_name = "pic/" + full_id +".png"
-    ele = browser.find_element(By.ID, f"{full_id}")
-    browser.execute_script("arguments[0].scrollIntoView(true);", ele)
-    ele.screenshot(img_name)
+    img_name = ss_element(full_id)
 
     await send_pic(img_name, capt)
 
     # download file yang ada pada post
-    if total_file == 1:
-        try:
-            await file_download(browser, full_id)
-        except Exception as e:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
-    elif total_file == 0 :
-        await send_msg("No File Attached")
-    elif total_file > 1:
-        try:
-            await files_download(browser, full_id, total_file)
-        except:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
+    await dl_file( total_file, full_id)
     
-    #json handler
-    data[f"{full_id}"] = {"jenis" : text_a[0], "Jurusan" : text_a[2], "Matkul" : text_a[3], "Dosen" : text_a[4], "Deskripsi" : desc2, "Waktu mulai" : text_b[(itext_b-4)], "Waktu mulai" : text_b[(itext_b-2)], "Status" : status, "Picname" : img_name} 
-    return data
-    
-async def diskusibot(browser, full_id, data):
+    if data:
+        #json handler
+        data[f"{full_id}"] = {"jenis" : parsered[0], "Jurusan" : parsered[1], "Matkul" : parsered[2], "Dosen" : parsered[3], "Deskripsi" : parsered[4], "Waktu mulai" : parsered[5], "Waktu mulai" : parsered[6], "Status" : status, "Picname" : img_name} 
+        return data
+
+async def diskusibot(full_id, data=False):
     # ambil text
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     main = soup.find("div", {"id": full_id})
-    status = main.attrs["class"][2]
     text_a = main.get_text(" | ", strip = True ).split(" | ")
-    sub = main.find("div", {"class": "post_content"})
-    text_b= sub.get_text(" | ", strip = True ).split(" | ")
+    status = main.attrs["class"][2]
     total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
-    itext_b = len(text_b)
-
-    if total_file > 0 :
-        total_file2 = total_file * 2 + 1
-        Node = itext_b - 11 - total_file2
-    else:
-        Node = itext_b - 11
-    
-    ##untuk mengantisipasi jika ada lebih dari 1 line pada deskripsi
-    desc =[]
-    for i in text_b[6:Node] : 
-        desc.append(i)
-    desc2 = "\n".join(desc)
+    parsered = diskusi_parser(text_a)
+    print(text_a)
 
     ## bot caption maker
-    capt = f"""**Jenis :** {text_a[1]}
-**Jurusan :** {text_a[3]}
-**Matkul :  **{text_a[4]}
-**Dosen : **{text_a[5]}
-**Indikator Kemampuan : **{text_b[1]}
-**Materi Perkuliahan : **{text_b[3]}
-**Bentuk Pembelajaran : **{text_b[5]}
-**Deskripsi : **{desc2}
+    capt = f"""**HTML-ID : ** `{full_id}`
+**Jenis :** {parsered[0]}
+**Jurusan :** {parsered[1]}
+**Matkul :  **{parsered[2]}
+**Dosen : **{parsered[3]}
+**Indikator Kemampuan : **{parsered[4]}
+**Materi Perkuliahan : **{parsered[5]}
+**Bentuk Pembelajaran : **{parsered[6]}
+**Deskripsi : **{parsered[7]}
 **Total File : **{total_file}
-**Waktu mulai  **{text_b[(itext_b-7)]}
-**Waktu selesai  **{text_b[(itext_b-5)]}
+**Waktu mulai  **{parsered[8]}
+**Waktu selesai  **{parsered[9]}
 **Status : **{status}
 """
 
     # take ss of element
-    img_name = "pic/"+full_id +".png"
-    ele = browser.find_element(By.ID, f"{full_id}")
-    browser.execute_script("arguments[0].scrollIntoView(true);", ele)
-    ele.screenshot(img_name)
+    img_name = ss_element(full_id)
 
     await send_pic(img_name, capt)
 
     # download file yang ada pada post
-    if total_file == 1:
-        try:
-            await file_download(browser, full_id)
-        except Exception as e:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
-    elif total_file == 0 :
-        await send_msg("No File Attached")
-    elif total_file > 1:
-        try:
-            await files_download(browser, full_id, total_file)
-        except:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
+    await dl_file( total_file, full_id)
+
+    if data:
+        data[f"{full_id}"] = {"Jenis" : parsered[0], "Jurusan" : parsered[1], "Matkul" : parsered[2], "Dosen" : parsered[3], "Indikator Kemampuan" : parsered[4], "Materi Perkuliahan" : parsered[5], "Bentuk Pembelajaran" : parsered[6], "Deskripsi" : parsered[7], "Waktu mulai" : parsered[8], "Waktu selesai" : parsered[9], "Status" : status, "Picname" : img_name}
+        return data
 
     # json handler
-    data[f"{full_id}"] = {"Jenis" : text_a[1], "Jurusan" : text_a[3], "Matkul" : text_a[4], "Dosen" : text_a[5], "Indikator Kemampuan" : text_b[1], "Materi Perkuliahan" : text_b[3], "Bentuk Pembelajaran" : text_b[5], "Deskripsi" : desc2, "Waktu mulai" : text_b[(itext_b-7)], "Waktu selesai" : text_b[(itext_b-5)], "Status" : status, "Picname" : img_name}
-    return data
 
-async def meetingbot(browser, full_id, data):
+async def meetingbot(full_id, data=False):
     # ambil text
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     main = soup.find("div", {"id": full_id})
     status = main.attrs["class"][2]
     text_a = main.get_text(" | ", strip = True ).split(" | ")
-    sub = main.find("div", {"class": "post_content"})
-    text_b= sub.get_text(" | ", strip = True ).split(" | ")
-    total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
-    itext_b = len(text_b)
 
-    if total_file > 0 :
-        total_file2 = total_file * 2 + 2
-        Node = itext_b - 13 - total_file2
-    else:
-        Node = itext_b - 13
-    
-    ##untuk mengantisipasi jika ada lebih dari 1 line pada deskripsi
-    desc =[]
-    for i in text_b[7:Node] : 
-        desc.append(i)
-    desc2 = "\n".join(desc)
+    parsered = meeting_parser (text_a)
+    link = "https://" + parsered[7]
+
+    total_file = len(browser.find_elements(By.XPATH,f'//*[@id="{full_id}"]/div[3]/p'))
 
     # print text
-    capt =f"""**Jenis :** {text_a[1]}
-**Jurusan :** {text_a[3]}
-**Matkul :  **{text_a[4]}
-**Dosen : **{text_a[5]}
-**Indikator Kemampuan : **{text_b[1]}
-**Materi Perkuliahan : **{text_b[3]}
-**Bentuk Pembelajaran : **{text_b[5]}
-**Link : **{text_b[6]}
-**Deskripsi : **{desc2}
+    capt = f"""**HTML-ID : ** `{full_id}`
+**Jenis :** {parsered[0]}
+**Jurusan :** {parsered[1]}
+**Matkul :  **{parsered[2]}
+**Dosen : **{parsered[3]}
+**Indikator Kemampuan : **{parsered[4]}
+**Materi Perkuliahan : **{parsered[5]}
+**Bentuk Pembelajaran : **{parsered[6]}
+**Link : ** {link}
+**Deskripsi : **{parsered[8]}
 **Total File : **{total_file}
-**Waktu mulai  **{text_b[(itext_b-11)]}
-**Waktu selesai  **{text_b[(itext_b-13)]}
+**Waktu mulai  **{parsered[9]}
+**Waktu selesai  **{parsered[10]}
 **Status : **{status}
 """
     # take ss of element
-    img_name = "pic/" +full_id +".png"
-    ele = browser.find_element(By.ID, f"{full_id}")
-    browser.execute_script("arguments[0].scrollIntoView(true);", ele)
-    ele.screenshot(img_name)
+    img_name = ss_element(full_id)
 
     await send_pic(img_name, capt)
 
     # download file yang ada pada post
-    if total_file == 1:
-        try:
-            await file_download(browser, full_id)
-        except Exception as e:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
-    elif total_file == 0 :
-        await send_msg("No File Attached")
-    elif total_file > 1:
-        try:
-            await files_download(browser, full_id, total_file)
-        except:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
+    await dl_file( total_file, full_id)
 
-    # json handler
-    data[f"{full_id}"] = {"Jenis" : text_a[1], "Jurusan" : text_a[3], "Matkul" : text_a[4], "Dosen" : text_a[5], "Indikator Kemampuan" : text_b[1], "Materi Perkuliahan" : text_b[3], "Bentuk Pembelajaran" : text_b[5], "lLink" : text_b[6], "Deskripsi" : desc2, "Waktu mulai" : text_b[(itext_b-11)], "Waktu selesai" : text_b[(itext_b-13)], "Status" : status, "Picname" : img_name}
-    return data
+    if data:
+        # json handler
+        data[f"{full_id}"] = {"Jenis":parsered[0], "Jurusan":parsered[1], "Matkul":parsered[2], "Dosen":parsered[3], "Indikator Kemampuan":parsered[4], "Materi Perkuliahan":parsered[5], "Bentuk Pembelajaran":parsered[6], "Link": link, "Deskripsi":parsered[8], "Total File":total_file, "Waktu mulai  ":parsered[9], "Waktu selesai  " :parsered[10], "Status":status, "Picname" : img_name}
+        return data
 
-async def forumbot(browser, full_id, data):
+
+async def forumbot(full_id, data=False):
     # ambil text
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     main = soup.find("div", {"id": full_id})
     text_a = main.get_text(" | ", strip=True).split(" | ")
-    dataparser = forumparser(text_a)
+    dataparser = forum_parser(text_a)
 
     sub = main.find("div", {"class": "post_content"})
     text_b = sub.get_text(" | ", strip=True).split(" | ")
@@ -215,14 +141,9 @@ async def forumbot(browser, full_id, data):
 
     status = "no-status-found"
 
-    if total_file > 0:
-        total_file2 = total_file * 2 + 2
-        Node = itext_b - 13 - total_file2
-    else:
-        Node = itext_b - 13
-
     # print text
-    capt = f"""**Jenis :** {dataparser[0]}
+    capt = f"""**HTML-ID : ** `{full_id}`
+**Jenis :** {dataparser[0]}
 **Jurusan :** {dataparser[1]}
 **Matkul :  **{dataparser[2]}
 **Nama Pengirim : **{dataparser[3]}
@@ -231,28 +152,102 @@ async def forumbot(browser, full_id, data):
 **Status : **{status}
 """
     # take ss of element
-    img_name = "pic/" + full_id + ".png"
-    ele = browser.find_element(By.ID, f"{full_id}")
-    browser.execute_script("arguments[0].scrollIntoView(true);", ele)
-    ele.screenshot(img_name)
+    img_name = ss_element(full_id)
 
     await send_pic(img_name, capt)
 
     # download file yang ada pada post
+    await dl_file( total_file, full_id)
+
+    if data:
+    # json handler
+        data[f"{full_id}"] = {"Jenis": dataparser[0], "Jurusan": dataparser[1], "Matkul": dataparser[2],"Nama Pengirim": dataparser[3], "Deskripsi": dataparser[4],"Status" : "no-status-found", "Picname": img_name}
+        return data
+
+async def materibot(full_id, data=False):
+    # ambil text
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    main = soup.find("div", {"id": full_id})
+    text_a = main.get_text(" | ", strip=True).split(" | ")
+    parsered = materi_parser(text_a)
+
+    total_file = len(browser.find_elements(By.XPATH, f'//*[@id="{full_id}"]/div[3]/p'))
+
+    status = "no-status-found"
+
+    # print text
+    capt = f"""**HTML-ID : ** `{full_id}`
+**Jenis :** {parsered[0]}
+**Jurusan :** {parsered[2]}
+**Matkul :  **{parsered[3]}
+**Nama Pengirim : **{parsered[1]}
+**Deskripsi : **{parsered[4]}
+**Total File : **{total_file}
+**Status : **{status}
+"""
+    # take ss of element
+    img_name = ss_element(full_id)
+
+    await send_pic(img_name, capt)
+
+    # download file yang ada pada post
+    await dl_file( total_file, full_id)
+    if data:
+        data[f"{full_id}"] = {"Jenis": parsered[0], "Jurusan": parsered[2], "Matkul": parsered[3],
+                          "Nama Pengirim": parsered[1], "Deskripsi": parsered[4],"Status" : "no-status-found", "Picname": img_name}
+        return data
+
+    # json handler
+
+
+async def videobot(full_id, data=False):
+        # pilih id yang mau dipakai
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    main = soup.find("div", {"id": full_id})
+    text_a = main.get_text(" | ", strip = True ).split(" | ")
+
+    status = main.attrs["class"][2]
+    parsered = video_parser(text_a)
+    
+    total_file = len(browser.find_elements(By.XPATH, f'//*[@id="{full_id}"]/div[3]/p'))
+    iframe_vid= browser.find_element(By.XPATH, f'//*[@id="{full_id}"]/div[3]/iframe')
+    vid_link = iframe_vid.get_attribute("src")
+
+    capt = f"""**HTML-ID : ** `{full_id}`
+**Jenis :** {parsered[0]}
+**Jurusan :** {parsered[1]}
+**Matkul :  **{parsered[2]}
+**Nama Pengirim : **{parsered[3]}
+**Deskripsi : **{parsered[4]}
+**Video Link : **{vid_link}
+**Status : **{status}
+"""
+    # take ss of element
+    img_name = ss_element(full_id)
+
+    await send_pic(img_name, capt)
+
+    # download file yang ada pada post
+    await dl_file( total_file, full_id)
+
+    if data:
+        data[f"{full_id}"] = {"Jenis": parsered[0], "Jurusan": parsered[2], "Matkul": parsered[3], "Nama Pengirim": parsered[1], "Deskripsi": parsered[4], "Video Link": vid_link, "Status": status, "Picname": img_name}
+        return data
+
+    # json handler
+
+
+async def dl_file(total_file, full_id):
     if total_file == 1:
-        try:
-            await file_download(browser, full_id)
-        except Exception as e:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
+        await file_download( full_id)
     elif total_file == 0:
         await send_msg("No File Attached")
     elif total_file > 1:
-        try:
-            await files_download(browser, full_id, total_file)
-        except:
-            await send_msg(f"An error occured, {traceback.format_exc()}")
+        await files_download( full_id, total_file)
 
-    # json handler
-    data[f"{full_id}"] = {"Jenis": dataparser[0], "Jurusan": dataparser[1], "Matkul": dataparser[2],
-                          "Nama Pengirim": dataparser[3], "Deskripsi": dataparser[4],"Status" : "no-status-found", "Picname": img_name}
-    return data
+def ss_element(full_id):
+    img_name = "pic/" + full_id + ".png"
+    ele = browser.find_element(By.ID, f"{full_id}")
+    browser.execute_script("arguments[0].scrollIntoView(true);", ele)
+    ele.screenshot(img_name)
+    return img_name
