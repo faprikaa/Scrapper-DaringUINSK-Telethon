3fc0d1af4d91aada4e01982b27a68aa3ce46417a
faprikaa <muammarm28@gmail.com>
2024-02-29 16:50:44 +0700

    Fix usage command

--- bot/commands/other.py
+++ bot/commands/other.py
@@ -3,6 +3,7 @@ from telethon import events
 
 from core.bot import bot
 from komen.komen import send_komen
+from util.config import CHAT_ID
 
 
 @bot.on(events.NewMessage(pattern='/komen(?:\s|$)(.*)'))
@@ -18,7 +19,7 @@ async def handler(event):
     ram = psutil.virtual_memory()[2]
     cpu = psutil.cpu_percent(4)
 
-    await bot.send_message(f"Total RAM yang terpakai sebesar : `{ram}%`\n Total CPU yang terpakai sebesar : `{cpu}%`")
+    await bot.send_message(CHAT_ID, f"Total RAM yang terpakai sebesar : `{ram}%`\n Total CPU yang terpakai sebesar : `{cpu}%`")
 # @bot.on(events.NewMessage(pattern='/list'))
 # async def handler(event):
 #     datas = load_saved_data()
